08a61905485628529bbae15692dfc88a1d9a39b1
James Norton <james.norton@me.com>
2016-04-29 13:22:38 -0700

    more skeltoning...basic directory structure, adding basic router and server

---
+++ routes/trail_router.js
@@ -0,0 +1,9 @@
+const Router = require('express').Router;
+const Trail = require(__dirname + '/../models/trail');
+var trailRouter = module.exports = Router();
+const bodyParser = require('body-parser').json();
+
+trailRouter.get('/', (req, res) => {
+
+  res.status(200).json({msg: 'Howdy Pardner!!'})
+});
