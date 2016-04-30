57bc549376b04dc7f4118c6f8af9aa77ad98dd09
James Norton <james.norton@me.com>
2016-04-30 12:30:18 -0700

    rewrite test to match Gene tests

--- routes/trail_routes.js
+++ routes/trail_routes.js
@@ -2,7 +2,7 @@ const Router = require('express').Router;
 const Trail = require(__dirname + '/../models/trail');
 const bodyParser = require('body-parser').json();
 const trailRouter = module.exports = new Router();
-const errorHandler = require('/../lib/db_error_handler');
+const errorHandler = require(__dirname + '/../lib/db_error_handler.js');
 
 trailRouter.get('/', (req, res) => {
   res.status(200).json({ msg: 'Howdy Pardner!!' });
@@ -19,7 +19,7 @@ trailRouter.get('/trail', (req, res) => {
 trailRouter.post('/trail', bodyParser, (req, res) => {
   var newTrail = new Trail(req.body);
   newTrail.save((err, data) => {
-    if err errorHandler();
+    if (err) errorHandler();
 
     res.status(200).json(data);
   });
@@ -29,7 +29,7 @@ trailRouter.put('/trail/:id', bodyParser, (req, res) => {
   var trailData = req.body;
   delete trailData._id;
   Trail.update({ _id: req.params.id }, trailData, (err) => {
-    if err errorHandler();
+    if (err) errorHandler();
 
     res.status(200).json({ msg: 'You have entered a new trail' });
   });
@@ -40,5 +40,5 @@ trailRouter.delete('/trail/:id', (req, res) => {
     if (err) errorHandler();
 
     res.status(200).json({ msg: 'Trail Deleted' });
-  })
-})
+  });
+});
