d59ade066c2ece222fad14010882d6511d0e5e7b
James Norton <james.norton@me.com>
2016-04-30 12:31:46 -0700

    uncomment code in logic file to match pull

--- logic/weather.js
+++ logic/weather.js
@@ -2,5 +2,5 @@ var ForecastIo = require('forecastio');
 
 var forecastIo = new ForecastIo('ce1e9e7c47068378251586a90ecb14cd');
 forecastIo.forecast('47.8669', -121.6780).then(function(data) {
-  // console.log(JSON.stringify(data, null, 2));
+  console.log(JSON.stringify(data, null, 2));
 });
