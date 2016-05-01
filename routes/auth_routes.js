ea0ca4fdcd10fd94a6cd3dda88290892b9169461
AlegriaForman <alegriaforman@gmail.com>
2016-04-30 20:52:16 -0700

    revise auth_routes.js

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -1,46 +1,44 @@
 const express = require('express');
-const jsonParser = require('body-parser').json();
+const bodyParser = require('body-parser').json();
 const basicHttp = require(__dirname + '/../lib/basic_http');
 const User = require(__dirname + '/../models/user');
-const handleDBError = require(__dirname + '/../lib/db_error_handler');
+const errorHandler = require(__dirname + '/../lib/db_error_handler');
+
 var authenticationRouter = module.exports = exports = express.Router();
 
-authenticationRouter.post('/signup', jsonParser, (req, res) => {
-  var newUser = new User();
-  var rbe = req.body.email;
-  if (rbe === '' || rbe.indexOf('@') < 0 || rbe.indexOf('.') < 0) {
-    return res.status(400).json({ msg: 'invalid email' });
-  }
-  if (req.body.password.length < 8) {
-    return res.status(400).json({ msg: 'invalid password' });
-  }
-  if (req.body.username.length === 0) {
-    return res.status(400).json({ msg: 'invalid username' });
-  }
+authenticationRouter.post('/signup', bodyParser, (req, res) => {
+  var password = req.body.password;
+  req.body.password = null;
 
-  User.find({ 'username': req.body.username }, (err, docs) => {
-    if (err) process.stdout.write(err);
-    if (docs.length > 0) {
-      return res.status(400).json({ msg: 'username taken' });
-    }
-    newUser.username = req.body.username;
-    newUser.authentication.email = req.body.email;
-    newUser.authentication.password = newUser.hashPassword(req.body.password);
-    newUser.save((err, data) => {
-      if (err) return handleDBError(err);
-      return res.status(200).json({ msg: 'good job!', token: data.generateToken() });
+  if (!password) return res.status(500).json({ msg: 'Please enter password.' });
+
+  var newUser = new User(req.body);
+  newUser.generateHash(password);
+  password = null;
+
+  newUser.save((err, user) => {
+    if (err) return res.status(500).json({ msg: 'Could not create user.' });
+
+    user.generateToken((err, token) => {
+      if (err) return res.status(500).json({ msg: 'Could not generate token, sign in later.' });
+
+      res.json({ token });
     });
   });
 });
+
 authenticationRouter.get('/signin', basicHttp, (req, res) => {
-  User.findOne({ 'username': req.basicHttp.username }, (err, data) => {
-    if (err) { return res.status(401).json({ result: 'db error' }); }
-    if (!data) {
-      return res.status(401).json({ msg: 'no user found' });
-    }
-    if (data.comparePassword(req.basicHttp.password) === false) {
-      return res.status(401).json({ msg: 'wrong password' });
-    }
-    return res.status(200).json({ token: data.generateToken() });
+  User.findOne({ username: req.auth.username }, (err, user) => {
+    if (err) return res.status(500).json({ msg: 'There is an error during sign-in, please try again!' });
+
+    if (!user) return res.status(500).json({ msg: 'Invalid username!' });
+
+    if (!user.compareHash(req.auth.password)) return res.status(500).json({ msg: 'Invalid password!'}); // eslint-disable-line
+
+    user.generateToken((err, token) => {
+      if (err) return res.status(500).json({ msg: 'Could not generate token, sign in later.' });
+
+      res.json({ token });
+    });
   });
 });
