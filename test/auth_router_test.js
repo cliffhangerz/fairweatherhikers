71f377ac3c1a2dfe311dc6125d6f32eed5a68447
Gene Troy <genetroy@hotmail.com>
2016-05-02 11:52:56 -0700

    zeroing the zerobuffer

--- test/auth_router_test.js
+++ test/auth_router_test.js
@@ -8,7 +8,6 @@ const main = require(__dirname + '/../_server');
 const origin = 'localhost:4000/api';
 var port = process.env.PORT || 3000;
 var testDb = process.env.MONGOLAB_URI || 'mongodb://localhost/db_test';
-// const zeroBuffer = require(__dirname + '/../lib/zero_buffer.js');
 
 describe('User Authentication: ', () => {
   before(() => {
@@ -172,12 +171,6 @@ describe('User Authentication: ', () => {
   //       expect(res.body.msg).to.eql('wrong password');
   //       done();
   //     });
-  //   });
-  //   it('should be able to zero out the buffer', (done) => {
-  //     var testBuffer = [3, 5, 57, 77777];
-  //     zeroBuffer(testBuffer);
-  //     expect(testBuffer.toString()).to.eql('\u0000\u0000\u0000\u0000');
-  //     done();
   //   });
   // });
 });
