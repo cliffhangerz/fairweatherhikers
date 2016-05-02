8e2d82b2efb72a59d2fb3c1dc2239bb285e5927a
AlegriaForman <alegriaforman@gmail.com>
2016-05-02 13:16:33 -0700

    revise trail_test.js

--- test/trail_test.js
+++ test/trail_test.js
@@ -3,13 +3,13 @@ const expect = chai.expect;
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 const request = chai.request;
+const errorHandler = require(__dirname + '/../lib/db_error_handler');
 const mongoose = require('mongoose');
-const port = process.env.PORT = 2000;
-process.env.MONGO_URI = 'mongodb://localhost/db_test';
-const app = require(__dirname + '/../_server');
+const port = process.env.PORT = 5555;
+const server = require(__dirname + '/../_server');
 const Trail = require(__dirname + '/../models/trail');
 const User = require(__dirname + '/../models/user');
-const errorHandler = require(__dirname + '/../lib/db_error_handler');
+
 process.env.APP_SECRET = 'testsecret';
 
 process.on('exit', () => {
@@ -18,13 +18,14 @@ process.on('exit', () => {
   }
 });
 
-describe('User server:', () => {
+describe('Trail Routing test', () => {
   before((done) => {
-    app.listen(port, 'mongodb://localhost/db_test', done);
+    server.listen(port, 'mongodb://localhost/trails_test_db', done);
+    console.log('server on port ' + port);
   });
 
   before((done) => {
-    var user = new User({ email: 'warrendoyle@gmail.com', password: 'appalachian' });
+    var user = new User({ email: 'beargrylls@email.com', password: 'huckleberry' });
     user.save((err, data) => {
       if (err) throw err;
       this.user = data;
@@ -39,40 +40,84 @@ describe('User server:', () => {
   after((done) => {
     mongoose.connection.db.dropDatabase(() => {
       mongoose.disconnect(() => {
-        app.close(done);
+        server.close(done);
       });
     });
   });
 
-  it('should work on a basic general request', (done) => {
+  it('should post a new trail', (done) => {
     request('localhost:' + port)
-    .get('/')
-    .end((err, res) => {
-      expect(err).to.eql(null);
-      expect(res.body.msg).to.eql('Yo de le he hoo!');
-      done();
-    });
-  });
-
-  it('should POST method for the droids', (done) => {
-    request('localhost:' + port)
-    .post('/api/trails')
+    .post('/api/trails/')
     .set('token', this.token)
-    .send({ loc:'Commonwealth Basin - Red Mtn. Pass', lat:47.4605, lon:121.3976, difficulty: 'hard', length:'7.2', time:5.5 }) //eslint-disable-line
+    .send({ loc: 'Test Trail',
+            lat: 47.2110,
+            lon: 122.3220,
+            difficulty: 'easy',
+            length: 5,
+            time: 2
+          })
     .end((err, res) => {
       expect(err).to.eql(null);
-      expect(res.body.loc).to.eql('Commonwealth Basin - Red Mtn. Pass');
-      expect(res.body.lat).to.eql(47.4605);
-      expect(res.body.lon).to.eql(121.3976);
-      expect(res.body.difficulty).to.eql('hard');
-      expect(res.body.length).to.eql('7.3');
-      expect(res.body.time).to.eql(5.5);
+      expect(res.status).to.eql(200);
+      expect(res.body.loc).to.eql('Test Trail');
+      expect(res.body.lat).to.eql(47.2110);
+      expect(res.body.lon).to.eql(122.3220);
+      expect(res.body.difficulty).to.eql('easy');
+      expect(res.body.length).to.eql('5');
+      expect(res.body.time).to.eql(2);
       done();
     });
   });
 
-  describe('GET method for the trails:', () => {
-    it('should get all trails with GET method', (done) => {
+  describe('Tests that need data in the database', () => {
+
+    beforeEach((done) => {
+      var newTrail = new Trail({ loc: 'Appalachian' });
+      newTrail.save((err, data) => {
+        if (err) return errorHandler(err, data);
+        this.testTrail = data;
+        done();
+      });
+    });
+
+    afterEach((done) => {
+      this.testTrail.remove(() => {
+        done();
+      });
+    });
+
+    it('should be able to PUT data for a trail', (done) => {
+      request('localhost:' + port)
+      .put('/api/trails/' + this.testTrail._id)
+      .send({ loc: 'Appalachian',
+              lat: 47.13245,
+              lon: 123.6370,
+              dificulty: 'easy',
+              length: '32',
+              time: 4
+            })
+      .end((err, res) => {
+        expect(err).to.eql(null);
+        expect(res.status).to.eql(200);
+        expect(res.body.msg).to.eql('You have changed trail information');
+        done();
+      });
+    });
+
+    it('should DELETE a trail', (done) => {
+      request('localhost:' + port)
+      .delete('/api/trails/' + this.testTrail._id)
+      .end((err, res) => {
+        expect(err).to.eql(null);
+        expect(res.status).to.eql(200);
+        expect(res.body.msg).to.eql('Trail Deleted');
+        done();
+      });
+    });
+  });
+
+  describe('the GET method', () => {
+    it('should get all the trails', (done) => {
       var token = this.token;
       request('localhost:' + port)
       .get('/api/trails')
@@ -84,44 +129,5 @@ describe('User server:', () => {
       });
     });
   });
-
-  describe('PUT and DELETE method for the trails: ', () => {
-    beforeEach((done) => {
-      var newTrail = new Trail({ loc:'Commonwealth Basin - Red Mtn. Pass', lat:47.4605, lon:121.3976, difficulty: 'hard', length:'7.2', time:5.5 }) //eslint-disable-line
-      newTrail.save((err, data) => {
-        if (err) return errorHandler(err);
-        this.trail = data;
-        done();
-      });
-    });
-
-    afterEach((done) => {
-      this.trail.remove(() => {
-        done();
-      });
-    });
-
-    it('should change difficulty to moderate and time to 4.5 hrs with PUT method', (done) => {
-      request('localhost:' + port)
-      .put('/api/trails/' + this.trail._id)
-      .send({ difficulty: 'moderate', time: 4.5 })
-      .end((err, res) => {
-        expect(err).to.eql(null);
-        expect(res.body.difficulty).to.eql('moderate');
-        expect(res.body.time).to.eql(4.5);
-        expect(res.body.msg).to.eql('You have changed trail information');
-        done();
-      });
-    });
-
-    it('should trail DELETE method', (done) => {
-      request('localhost:' + port)
-      .delete('/api/trails/' + this.trail._id)
-      .end((err, res) => {
-        expect(err).to.eql(null);
-        expect(res.body.msg).to.eql('Trail Deleted');
-        done();
-      });
-    });
-  });
 });
+
