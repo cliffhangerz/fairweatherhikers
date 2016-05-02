958afec4cd3b8361a05fe15e69a9ac272873fb2e
AlegriaForman <alegriaforman@gmail.com>
2016-05-01 23:28:10 -0700

    revise weather.js

--- logic/weather.js
+++ logic/weather.js
@@ -1,5 +1,4 @@
 const ForecastIo = require('forecastio');
-const fs = require('fs');
 
 var forecastIo = new ForecastIo('ce1e9e7c47068378251586a90ecb14cd');
 forecastIo.forecast('47.8669', -121.6780).then(function(data) {
