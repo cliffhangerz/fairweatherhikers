c24be1bca8e4e1cfa2c517fda8ea67ddecb77105
AlegriaForman <alegriaforman@gmail.com>
2016-05-03 21:57:57 -0700

    revise auth_routes

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -20,32 +20,29 @@ authenticationRouter.post('/signup', bodyParser, (req, res) => {
     }
   });
 
-  var newUser = new User();
+  var newUser = new User(req.body);
+  req.body.email = null;
+  var password = req.body.password;
+  newUser.generateHash(password);
+  req.body.password = null;
+  password = null;
 
-  newUser.save((err, data) => {
-    if (err) return handleDBError(err);
-    this.newUser = data;
-    newUser.generateToken((err, token) => {
-      if (err) throw err;
-      this.token = token;
-      newUser.email = req.body.email;
-      newUser.password = newUser.generateHash(req.body.password);
-    // req.body.password = null;
-    return res.status(200).json({ msg: 'good signup', token: token }); // eslint-disable-line
+  newUser.save((err, user) => {
+    if (err) return res.status(400).json({ msg: 'Could not create user.' });
+
+    user.generateToken((err, token) => {
+      if (err) return res.status(400).json({ msg: 'Could not generate token, sign in later.' });
+      return res.status(200).json({ msg: 'good signup', token: token });
     });
   });
 });
 
 authenticationRouter.get('/signin', basicHttp, (req, res) => {
-  User.findOne({ email: req.email }, (err, data) => {
-    if (err) return res.status(401).json({ msg: 'sign-in error' });
-    if (!data) return res.status(401).json({ msg: 'Invalid email!' });
-
-    if (data.compareHash(req.basicHttp.password) === false) {
-      return res.status(401).json({ msg: 'wrong password' });
-    }
-
-    data.generateToken((err, token) => {
+  User.findOne({ email: req.auth.email }, (err, user) => {
+    if (err) return res.status(401).json({ msg: 'There is an error during sign-in, please try again!' }); //eslint-disable-line
+    if (!user) return res.status(401).json({ msg: 'Invalid email!' });
+    if (!user.compareHash(req.auth.password)) return res.status(401).json({ msg: 'Invalid password!'}); // eslint-disable-line
+    user.generateToken((err, token) => {
       if (err) return res.status(401).json({ msg: 'Could not generate token, sign in later.' });
       res.json({ token });
     });
