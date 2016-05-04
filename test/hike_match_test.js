fae3ad972dbf687ec4bd0961e11957e524c01774
James Norton <james.norton@me.com>
2016-05-03 18:15:54 -0700

    modify hike match to be more module, update hike atch test to use server, not test-server

--- test/hike_match_test.js
+++ test/hike_match_test.js
@@ -2,16 +2,30 @@ const chai = require('chai');
 const expect = chai.expect;
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
-// const request = chai.request;
+const request = chai.request;
+const errorHandler = require(__dirname + '/../lib/db_error_handler');
+const mongoose = require('mongoose');
 // const Trail = require(__dirname + '/../models/trail');
-const matchResults = rquire(__dirname + '/../lib/dataresults');
-
-const main = require(__dirname + '/test_server');
-const origin = 'localhost:4000/api';
+const goodHike = require(__dirname + '/../lib/goodHike');
+const port = process.env.PORT = 6666;
+const server = require(__dirname + '/../_server');
 
 const ForecastIo = require('forecastio');
 
+process.env.APP_SECRET = 'secrettest';
+
+process.on('exit', () => {
+  if (mongoose.connection.db) {
+    mongoose.connection.db.dropDatabase();
+  }
+});
+
 describe('API Call test', () => {
+  before((done) => {
+    server.listen(port, 'mongodb://localhost/hike_match_test_db', done);
+    console.log('server on port ' + port);
+  })
+
   it('should make a REST API call and return weather data ', (done) => {
     var forecastIo = new ForecastIo('ce1e9e7c47068378251586a90ecb14cd');
     forecastIo.forecast('39.2530', '-118.4210').then((data) => {
@@ -19,34 +33,5 @@ describe('API Call test', () => {
       expect(data.daily.data[0].precipProbability).to.be.an('number');
       done();
     });
-  });
-});
-
-
-describe('Hike Match test', () => {
-  const testData = {
-    latitude: 39.2530,
-    longitude: -118.4210,
-    daily: {
-      data: [
-        {
-          "precipProbability": .22
-        },
-        {
-          "precipProbability": .70
-        },
-        {
-          "precipProbability": .00
-        }
-      ]
-    }
-  }
-
-
-  it('should process and match only day one and day three', () => {
-    // var result = goodHike(testData);
-
-    expect(err).to.eql(null);
-    expect(result).to.not.eql(null);
   });
 });
